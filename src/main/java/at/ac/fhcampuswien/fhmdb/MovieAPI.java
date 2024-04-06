4020a7cd1298b47d37e2e8859965d0c2c845f31b
plukesch <patrick.lukesch@chello.at>
2024-04-06 15:38:06 +0200

    module-info bearbeitet für okhttp und gson

--- src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
@@ -5,6 +5,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -58,9 +61,9 @@ public class MovieAPI {
         }
     }
 
-    /*public List<Movie> parseMoviesJson(String json) {
+    public List<Movie> parseMoviesJson(String json) {
         Gson gson = new Gson();
         Type movieListType = new TypeToken<List<Movie>>(){}.getType();
         return gson.fromJson(json, movieListType);
-    }*/
+    }
 }
